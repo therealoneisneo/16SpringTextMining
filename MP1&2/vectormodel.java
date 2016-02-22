dd3b5d12ade74579e7454fb022de5942e2ff0850
Frank <therealoneisneo@gmail.com>
2016-02-22 00:45:51 -0500

    import problem

---
+++ MP1&2/vectormodel.java
@@ -0,0 +1,11 @@
+import src.analyzer.DocAnalyzer.analyzer;
+
+
+public class vectormodel
+{
+	public static void main(String[] args) 
+	{
+		test = new DocAnalyzer();
+		test.LoadStopwords("init_stop_words.txt");
+	}
+}
